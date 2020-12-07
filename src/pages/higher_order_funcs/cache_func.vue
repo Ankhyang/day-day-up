be42b8cd3bbc0e292cdae28d71fd586703235251
Ankhyang <ankh3690@163.com>
2020-12-07 18:18:43 +0800

    高阶函数使用

--- src/pages/higher_order_funcs/cache_func.vue
+++ src/pages/higher_order_funcs/cache_func.vue
@@ -1,10 +1,32 @@
 <script type="text/jsx">
 export default {
     name: 'cache_func',
+    methods: {
+        memorize(fn) {
+            const cache = Object.create(null);
+            return function(...args) {
+                const _args = JSON.stringify(args);
+                return cache[_args] || (cache[_args] = fn.apply(fn, args))
+            }
+        },
+        caculate(a, b) {
+            return a * b
+        }
+    },
     render() {
+        const { memorize, caculate } = this
+        const cacFunc = memorize(caculate)
+        const num1 = cacFunc(2, 3)
+        const num2 = cacFunc(3, 4)
+        const num3 = cacFunc(2, 3)
+        const num4 = caculate(5, 6)
         return (
             <div>
                 <p>我是缓存函数</p>
+                <p>{num1}</p>
+                <p>{num2}</p>
+                <p>{num3}</p>
+                <p>{num4}</p>
             </div>
         )
     }
--- src/pages/higher_order_funcs/inertia_func.vue
+++ src/pages/higher_order_funcs/inertia_func.vue
@@ -1,6 +1,26 @@
 <script type="text/jsx">
 export default {
     name: 'inertia_func',
+    methods: {
+        // 根据不同的逻辑判断 覆盖调用函数逻辑
+        addHandler(element, type, handler) {
+            if (element.addEventListener) {
+                addHandler = function (element, type, handler) {
+                    element.addEventListener(type, handler, false);
+                }
+            } else if (element.attachEvent) {
+                addHandler = function (element, type, handler) {
+                    element.attachEvent("on" + type, handler);
+                }
+            } else {
+                addHandler = function (element, type, handler) {
+                    element["on" + type] = handler;
+                }
+            }
+            // 保证首次调用能正常执行监听
+            return addHandler(element, type, handler)
+        }
+    },
     render() {
         return (
             <div>
