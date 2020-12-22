9dbc29756a85092407b9e88065da62b05232d65b
Ankhyang <ankh3690@163.com>
2020-12-22 17:58:41 +0800

    样式技巧练习

---
+++ src/pages/css/content_attr.vue
@@ -0,0 +1,40 @@
+<script type="text/jsx">
+// 利用伪类的content结合attr展示内容
+
+export default {
+    name: 'contentAttr',
+    data() {
+        return {
+            colors: {
+                '姨妈红': '#f66',
+                '基佬紫': '#66f',
+                '箩底橙': '#f90',
+                '姣婆蓝': '#09f',
+                '大粪青': '#9c3',
+                '原谅绿': '#3c9'
+            }
+        }
+    },
+    methods: {
+        renderEle() {
+            return (
+                Object.keys(this.colors).map(item => {
+                    let value = this.colors[item]
+                    return <li data-name={item} style={{'backgroundColor': value}}></li>
+                })
+            )
+        }
+    },
+    render() {
+        return (
+            <ul class="hover-tips">
+                {this.renderEle()}
+            </ul>
+        )
+    }    
+}
+</script>
+
+<style lang="less" scoped>
+    
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -8,6 +8,10 @@ import InertiaFunc from '../higher_order_funcs/inertia_func'
 import CssBorder from '../css/css_border'
 // 自定义指令
 import Directive from '../custome_directives'
+// 伪类元素结合counter计数器练习
+import FloatingBall from "../css/content_floating_ball"
+// 伪类元素结合attr练习
+import ContentAttr from "../css/content_attr";
 export default {
     name: 'home',
     components: {
@@ -15,12 +19,14 @@ export default {
         CacheFunc,
         PartialFunc,
         InertiaFunc,
-        CssBorder
+        CssBorder,
+        FloatingBall,
+        ContentAttr
     },
     render() {
         return (
             <div>
-                <Directive/>
+                <ContentAttr/>
             </div>
         )
     }
@@ -28,5 +34,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+    div{
+        margin: 100px auto;
+    }
 </style>
