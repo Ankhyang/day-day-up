fefd9f24a602fc4a08f4c0a54430bd1997266aaf
Ankhyang <ankh3690@163.com>
2020-12-22 18:01:13 +0800

    样式设计

--- src/pages/css/content_attr.vue
+++ src/pages/css/content_attr.vue
@@ -36,5 +36,56 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    
+.hover-tips {
+    display: flex;
+    justify-content: space-between;
+    width: 200px;
+    li {
+        position: relative;
+        padding: 2px;
+        border: 2px solid transparent;
+        border-radius: 100%;
+        width: 24px;
+        height: 24px;
+        background-clip: content-box;
+        cursor: pointer;
+        transition: all 300ms;
+        &::before,
+        &::after {
+            position: absolute;
+            left: 50%;
+            bottom: 100%;
+            opacity: 0;
+            transform: translate3d(0, -30px, 0);
+            transition: all 300ms;
+        }
+        &::before {
+            margin: 0 0 12px -35px;
+            border-radius: 5px;
+            width: 70px;
+            height: 30px;
+            background-color: rgba(#000, .5);
+            line-height: 30px;
+            text-align: center;
+            color: #fff;
+            content: attr(data-name);
+        }
+        &::after {
+            margin-left: -6px;
+            border: 6px solid transparent;
+            border-top-color: rgba(#000, .5);
+            width: 0;
+            height: 0;
+            content: "";
+        }
+        &:hover {
+            &::before,
+            &::after {
+                opacity: 1;
+                transform: translate3d(0, 0, 0);
+            }
+        }
+    }
+}
+
 </style>
